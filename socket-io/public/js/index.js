650681cff90c5a6e6858cd411ded9f70f39e48ed
Alejandro Vargas <alexivamo@hotmail.com>
2018-05-21 09:31:24 -0500

    Add form to submit messages

--- socket-io/public/js/index.js
+++ socket-io/public/js/index.js
@@ -1,22 +1,28 @@
 var socket = io()
+
 socket.on('connect', function () {
-    console.log('Connected to server')
-    socket.emit('createMessage', {
-      from: 'Alex',
-      text: 'This is client to server text'
-    })
+  console.log('Connected to server')
 })
+
 socket.on('disconnect', function () {
-    console.log('Disconnected from server')
+  console.log('Disconnected from server')
 })
 
 socket.on('newMessage', function (message) {
-  console.log('newMessage: ' + JSON.stringify(message))
+  console.log('newMessage', message)
+  var li = jQuery('<li></li>')
+  li.text(`${message.from}: ${message.text}`)
+
+  jQuery('#messages').append(li)
 })
 
-socket.on('createMessage', {
-  from: 'Frank',
-  text: 'Hi',
-}, function (data) {
-  console.log('Got it', data)
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault()
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
+
+  })
 })
