6ca2bcf2ad18a0104118d2a71de628557529a263
Alejandro Vargas de la Mora <avargaz@Alejandros-MacBook-Pro.local>
2018-05-21 08:11:00 -0500

    Broadcast new connected users

--- socket-io/server/server.js
+++ socket-io/server/server.js
@@ -14,9 +14,14 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
     console.log('New user connected')
     socket.emit('newMessage', {
-        from: 'John',
+        from: 'Admin',
         text: 'This is a text',
-        createdAt: 123
+        createdAt: new Date().getTime()
+    })
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New User joined',
+        createdAt: new Date().getTime()
     })
     socket.on('createMessage', (message) => {
         console.log('createMessage', message)
