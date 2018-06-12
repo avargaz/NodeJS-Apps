395e65e03571a75479187cc7f353320eed77849f
Alejandro Vargas de la Mora <alexivamo@hotmail.com>
2018-06-12 12:15:26 -0400

    Change index-chat.js

--- socket-io/public/js/chat.js
+++ socket-io/public/js/chat.js
@@ -34,10 +34,7 @@ socket.on('newMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
-<<<<<<< HEAD
   scrollToBottom();
-=======
->>>>>>> 97b56d5db37e73c97b4a77d4fa7a3b28a0ca89c4
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -50,10 +47,7 @@ socket.on('newLocationMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
-<<<<<<< HEAD
   scrollToBottom();
-=======
->>>>>>> 97b56d5db37e73c97b4a77d4fa7a3b28a0ca89c4
 });
 
 jQuery('#message-form').on('submit', function (e) {
